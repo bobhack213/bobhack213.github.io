5b241ea109850d499ef8a3882de97f7fc6a11658
bobhack213 <104632272+bobhack213@users.noreply.github.com>
2023-06-25 15:46:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+// Function to handle incoming requests
+function handleRequest(request, response) {
+  // Get the user's information
+  const userInfo = {
+    ip: request.headers['x-forwarded-for'] || request.connection.remoteAddress,
+    userAgent: request.headers['user-agent'],
+    timestamp: new Date().toISOString(),
+    url: request.url, // Track the requested URL
+  };
+
+  // Save the user's information to a file
+  fs.appendFile('C:\\Users\\olive\\OneDrive\\Skrivbord\\users_data.txt', JSON.stringify(userInfo) + '\n', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log('User information logged successfully!');
+    }
+  });
+
+  // Respond to the user's request
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello, thanks for visiting!');
+}
+
+// Start the server
+const http = require('http');
+const server = http.createServer(handleRequest);
+const port = 3000; // Replace with your desired port number
+const hostname = '127.0.0.1'; // Replace with your desired hostname
+
+server.listen(port, hostname, () => {
+  console.log(`Server is running and listening on http://${hostname}:${port}`);
+});
