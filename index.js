54f574de8edc00c9988c93a75ca8ba58c2f09a9a
bobhack213 <104632272+bobhack213@users.noreply.github.com>
2023-06-25 16:12:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
+const express = require('express');
 const fs = require('fs');
-const http = require('http');
-const url = require('url');
 
-// Function to handle incoming requests
-function handleRequest(request, response) {
-  // Get the user's information
-  const userInfo = {
-    ip: request.headers['x-forwarded-for'] || request.connection.remoteAddress,
-    userAgent: request.headers['user-agent'],
-    timestamp: new Date().toISOString(),
-    url: request.url,
-  };
+const app = express();
+const port = 3000;
 
-  // Save the user's information to a file
-  fs.appendFile("C:\Users\olive\OneDrive\Skrivbord\users data.txt", JSON.stringify(userInfo) + '\n', (err) => {
+app.get('/', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const timestamp = new Date().toISOString();
+  
+  // Save the IP address and timestamp to a file
+  fs.appendFile('ip_log.txt', `IP: ${ip} - Timestamp: ${timestamp}\n`, (err) => {
     if (err) {
       console.error('Error writing to file:', err);
-    } else {
-      console.log('User information logged successfully!');
     }
   });
 
-  // Respond to the user's request
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello, thanks for visiting!');
-}
+  res.send('IP has been logged!');
+});
 
-// Start the server
-const server = http.createServer(handleRequest);
-const port = 3000;
-
-server.listen(port, () => {
-  console.log(`Server is running and listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
